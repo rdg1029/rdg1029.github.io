8891efdb459aea0cd4ed93fc9429f338de640f6e
rdg1029 <wcle3456@gmail.com>
2024-09-07 21:15:25 +0900

    Deploy

---
+++ out/_next/static/d9gYn7ZrMiOIoi1UtW_Wo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/27b4d6b789fddb24.css","static/chunks/pages/index-48779c1f16eebacb.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
