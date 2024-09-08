87fdd271ad790ddd35858fc2f461fb2b39e17ee4
rdg1029 <wcle3456@gmail.com>
2024-09-08 14:15:10 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
+import createMDX from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   trailingSlash: true,
   output: 'export',
   images: {
@@ -8,4 +11,9 @@ const nextConfig = {
   }
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
