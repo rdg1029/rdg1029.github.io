5c085de3d98c29827d437948f78a8203ed86c64f
rdg1029 <wcle3456@gmail.com>
2024-09-12 21:27:31 +0900

    Update pages/index.tsx : Show all mdx files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
-export default function Home() {
+import { useRouter } from 'next/router';
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import { MDX_PATH, mdxFilePaths } from "@/lib/mdx-utils";
+
+export const getStaticProps = (async () => {
+
+  return { props: { MDX_PATH, mdxFilePaths } }
+}) satisfies GetStaticProps<{MDX_PATH: string, mdxFilePaths: string[]}>
+
+export default function Home({MDX_PATH, mdxFilePaths}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
   return (
     <div>
+      <p className="text-4xl">Posts</p>
+      {mdxFilePaths.map((fileName => {
+        const title = fileName.replace(".mdx", "");
+        return <p><button onClick={() => router.push(`/posts/${title}`)}>{title}</button></p>
+      }))}
     </div>
   );
 }
