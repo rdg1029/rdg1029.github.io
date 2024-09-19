a6b39fef35eff7ed4824f2d37f71f9d10824aeff
rdg1029 <wcle3456@gmail.com>
2024-09-19 18:24:42 +0900

    Update pages/index.tsx : Apply tag list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,16 @@ import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 import { mdxFilePaths, getMdxData, FrontMatter } from "@/lib/mdx-utils";
 import PostItem from '@/components/post-item';
 
-export const getStaticProps: GetStaticProps<{ postList: FrontMatter[] }> = (async () => {
+export const getStaticProps: GetStaticProps<{ postList: FrontMatter[], tagList: string[] }> = (async () => {
   const postList = await Promise.all(mdxFilePaths.map(async (fileName) => {
     const mdxSource = await getMdxData(`${fileName}`);
     return Object.assign(mdxSource.frontmatter, { id: fileName.replace(".mdx", "") }) as FrontMatter;
   }));
-  return { props: { postList } }
+  const tagList = postList.map((data) => data.tag).reduce((acc, cur) => acc.concat(cur));
+  return { props: { postList, tagList } }
 });
 
-export default function Home({ postList }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home({ postList, tagList }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
   return (
     <div className='flex flex-col max-w-screen-xl min-w-80 w-full m-auto'>
@@ -25,17 +26,23 @@ export default function Home({ postList }: InferGetStaticPropsType<typeof getSta
           </div>
         </nav>
       </header>
-      <main className='max-w-screen-md w-full m-auto p-5'>
-        <p className="text-4xl">Posts</p>
-        {postList.map((data, idx) => {
-          return (
-            <PostItem key={idx} data={data} onClick={() => router.push(`/posts/${data.id}`)} />
-          )
-        })}
-        {/* {mdxFilePaths.map(((fileName, idx) => {
-          const title = fileName.replace(".mdx", "");
-          return <p key={idx}><button onClick={() => router.push(`/posts/${title}`)}>{title}</button></p>
-        }))} */}
+      <main className='flex flex-row gap-x-4 max-w-screen-xl w-full m-auto p-5'>
+        <div className='grow'>
+          <p className="text-3xl">Posts</p>
+          {postList.map((data, idx) => {
+            return (
+              <PostItem key={idx} data={data} onClick={() => router.push(`/posts/${data.id}`)} />
+            )
+          })}
+        </div>
+        <div className='grow-0'>
+          <p className="text-3xl">Tags</p>
+          <div className='flex flex-wrap gap-2'>
+            {Array.from(new Set(tagList)).map((tagName, idx) => {
+              return <p className='rounded-xl bg-neutral-200 text-sm font-semibold py-0.5 px-1' key={idx}>{tagName}</p>
+            })}
+          </div>
+        </div>
       </main>
       <footer className='p-5 border-2 border-x-0 border-b-0 border-t-slate-200'>
         <div><p>EMAIL: wcle3456@gmail.com</p></div>
