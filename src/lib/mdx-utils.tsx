87e7b7ac6d5eb5f16baaff9b707650dad1289e75
rdg1029 <wcle3456@gmail.com>
2024-09-19 17:13:37 +0900

    Update mdx-utils.tsx : Define frontmatter type

--- src/lib/mdx-utils.tsx
+++ src/lib/mdx-utils.tsx
@@ -2,10 +2,18 @@ import fs from "fs";
 import path from "path";
 import { serialize } from 'next-mdx-remote/serialize'
 
+export type FrontMatter = {
+    id: string;
+    title: string;
+    description: string;
+    date: string;
+    tag: string[];
+}
+
 export const MDX_PATH = path.join(process.cwd(), 'src/markdown');
 export const mdxFilePaths = fs.readdirSync(MDX_PATH);
 export async function getMdxData(fileName: string) {
     const source = fs.readFileSync(path.join(MDX_PATH, fileName), 'utf-8');
-    const mdxSource = await serialize(source, {parseFrontmatter: true});
+    const mdxSource = await serialize(source, { parseFrontmatter: true });
     return mdxSource;
 }
