74479447761e04b49d8b4f6186704f93b79cba6a
rdg1029 <wcle3456@gmail.com>
2024-09-19 17:17:19 +0900

    Update posts/[id].tsx : Edit div width

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,11 +1,11 @@
 import type { InferGetStaticPropsType, GetStaticProps, GetStaticPaths } from 'next'
 import { mdxFilePaths, getMdxData } from "@/lib/mdx-utils";
-import { MDXRemote} from 'next-mdx-remote'
+import { MDXRemote } from 'next-mdx-remote'
 import MdxLayout from "@/components/mdx-layout";
 
 export const getStaticPaths: GetStaticPaths = async () => {
     return {
-        paths: mdxFilePaths.map(fileName => ({params: {id: fileName.replace(".mdx", "")}})),
+        paths: mdxFilePaths.map(fileName => ({ params: { id: fileName.replace(".mdx", "") }})),
         fallback: false
     }
 }
@@ -15,19 +15,19 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     const mdxSource = await getMdxData(`${id}.mdx`);
     const content = mdxSource;
     const data = mdxSource.frontmatter
-    return { props: {content, data} }
+    return { props: { content, data } }
 }
 
 export default function Post({ content, data }: InferGetStaticPropsType<typeof getStaticProps>) {
     // const router = useRouter();
     // const { id } = router.query;
     return (
-        <>
-        <p>Title: {data.title}</p>
-        <p>Tag: {data.tag}</p>
-        <MdxLayout>
-            <MDXRemote {...content} />
-        </MdxLayout>
-        </>
+        <div className='flex flex-col max-w-screen-md min-w-80 w-full m-auto'>
+            <p>Title: {data.title}</p>
+            <p>Tag: {data.tag}</p>
+            <MdxLayout>
+                <MDXRemote {...content} />
+            </MdxLayout>
+        </div>
     );
 }
