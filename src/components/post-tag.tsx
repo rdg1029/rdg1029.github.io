9a33a7ddd503cc09d59ca92f76d2fde92f930723
rdg1029 <wcle3456@gmail.com>
2024-09-19 20:53:55 +0900

    Add post-tag.tsx

---
+++ src/components/post-tag.tsx
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction } from "react";
+import { FrontMatter } from "@/lib/mdx-utils";
+
+type Props = {
+    tagList: string[];
+    postListAll: FrontMatter[];
+    setPostList: Dispatch<SetStateAction<FrontMatter[]>>
+}
+
+export default ({tagList, postListAll, setPostList}: Props) => {
+    return (
+        <>
+            <p className="text-3xl">Tags</p>
+            <div className='flex flex-wrap gap-2 max-w-48 mt-3'>
+                <button
+                    className='rounded-xl bg-neutral-200 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
+                    onClick={() => setPostList(postListAll)}>ALL</button>
+                {tagList.map((tagName, idx) => {
+                    return (
+                        <button
+                            className='rounded-xl bg-neutral-200 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
+                            key={idx}
+                            onClick={() => setPostList(postListAll.filter(data => data.tag.includes(tagName)))}>{tagName}</button>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
