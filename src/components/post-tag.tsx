2b2247d98cbe40b370e8a35a4b54d41f5179af0d
rdg1029 <wcle3456@gmail.com>
2024-09-20 21:11:13 +0900

    post-tag.tsx : Edit props & click event

--- src/components/post-tag.tsx
+++ src/components/post-tag.tsx
@@ -1,26 +1,32 @@
 import { Dispatch, SetStateAction } from "react";
-import { FrontMatter } from "@/lib/mdx-utils";
 
 type Props = {
+    tag: string;
     tagList: string[];
-    postListAll: FrontMatter[];
-    setPostList: Dispatch<SetStateAction<FrontMatter[]>>
+    setTag: Dispatch<SetStateAction<string>>;
+    updatePostList: ({orderLatest, tagName}: {orderLatest?: boolean, tagName?: string}) => void;
 }
 
-export default ({tagList, postListAll, setPostList}: Props) => {
+export default ({tag, tagList, setTag, updatePostList}: Props) => {
     return (
         <>
             <p className="text-3xl">Tags</p>
             <div className='flex flex-wrap gap-2 max-w-48 m-3'>
                 <button
-                    className='rounded-xl bg-neutral-200 hover:bg-neutral-300 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
-                    onClick={() => setPostList(postListAll)}>ALL</button>
+                    className={`rounded-xl ${tag === "ALL" ? 'bg-neutral-400' : 'bg-neutral-200 hover:bg-neutral-300'} text-sm font-semibold py-0.5 px-1`}
+                    onClick={() => {
+                        setTag("ALL");
+                        updatePostList({tagName: "ALL"});
+                    }}>ALL</button>
                 {tagList.map((tagName, idx) => {
                     return (
                         <button
-                            className='rounded-xl bg-neutral-200 hover:bg-neutral-300 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
+                            className={`rounded-xl ${tagName === tag ? 'bg-neutral-400' : 'bg-neutral-200 hover:bg-neutral-300'} text-sm font-semibold py-0.5 px-1`}
                             key={idx}
-                            onClick={() => setPostList(postListAll.filter(data => data.tag.includes(tagName)))}>{tagName}</button>
+                            onClick={() => {
+                                setTag(tagName);
+                                updatePostList({tagName: tagName});
+                            }}>{tagName}</button>
                     )
                 })}
             </div>
