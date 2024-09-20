6473c2d03a8f1f956201ae05b24409e275aea15b
rdg1029 <wcle3456@gmail.com>
2024-09-20 15:00:00 +0900

    Update post-tag.tsx : Edit css

--- src/components/post-tag.tsx
+++ src/components/post-tag.tsx
@@ -11,14 +11,14 @@ export default ({tagList, postListAll, setPostList}: Props) => {
     return (
         <>
             <p className="text-3xl">Tags</p>
-            <div className='flex flex-wrap gap-2 max-w-48 mt-3'>
+            <div className='flex flex-wrap gap-2 max-w-48 m-3'>
                 <button
-                    className='rounded-xl bg-neutral-200 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
+                    className='rounded-xl bg-neutral-200 hover:bg-neutral-300 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
                     onClick={() => setPostList(postListAll)}>ALL</button>
                 {tagList.map((tagName, idx) => {
                     return (
                         <button
-                            className='rounded-xl bg-neutral-200 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
+                            className='rounded-xl bg-neutral-200 hover:bg-neutral-300 focus:bg-neutral-400 text-sm font-semibold py-0.5 px-1'
                             key={idx}
                             onClick={() => setPostList(postListAll.filter(data => data.tag.includes(tagName)))}>{tagName}</button>
                     )
