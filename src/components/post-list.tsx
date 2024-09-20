b4d5a727c8a7ceb930e3d4f75b2534558b8b4076
rdg1029 <wcle3456@gmail.com>
2024-09-20 21:10:35 +0900

    Update post-list.tsx : Edit props & click event

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,16 +1,34 @@
 import { useRouter } from 'next/router';
 import { FrontMatter } from "@/lib/mdx-utils"
 import PostItem from "./post-item";
+import { Dispatch, SetStateAction, useState } from 'react';
 
 type Props = {
     list: FrontMatter[];
+    latestState: [boolean, Dispatch<SetStateAction<boolean>>];
+    updatePostList: ({orderLatest, tagName}: {orderLatest?: boolean, tagName?: string}) => void;
 }
 
-export default ({list}: Props) => {
+export default ({list, latestState, updatePostList}: Props) => {
     const router = useRouter();
+    const CSS_BTN_ACTIVE = 'font-extrabold underline decoration-2';
+    const [latest, setLatest] = latestState;
+
     return (
         <div className='max-w-screen-lg'>
             <p className="text-3xl">Posts</p>
+            <div className='flex flex-row justify-end gap-x-2'>
+                <button className={latest ? CSS_BTN_ACTIVE : ''} onClick={() => {
+                    if (latest) return;
+                    setLatest(true);
+                    updatePostList({orderLatest: true});
+                }}>Latest</button>
+                <button className={!latest ? CSS_BTN_ACTIVE : ''} onClick={() => {
+                    if (!latest) return;
+                    setLatest(false);
+                    updatePostList({orderLatest: false});
+                }}>Oldest</button>
+            </div>
             {list.map((data, idx) => <PostItem key={idx} data={data} onClick={() => router.push(`/posts/${data.id}`)} />)}
         </div>
     )
