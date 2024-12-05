f60e742a63c7c5677b922d9374aab95036b8645a
rdg1029 <wcle3456@gmail.com>
2024-12-05 15:51:43 +0900

    Update mdx-utils.tsx : Apply rehype-pretty-code

--- src/lib/mdx-utils.tsx
+++ src/lib/mdx-utils.tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { serialize } from 'next-mdx-remote/serialize'
+import rehypePrettyCode from 'rehype-pretty-code';
 
 export type FrontMatter = {
     id: string;
@@ -12,10 +13,21 @@ export type FrontMatter = {
     tag: string[];
 }
 
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+    theme: 'one-dark-pro',
+    keepBackground: false,
+};
+
 export const MDX_PATH = path.join(process.cwd(), 'src/markdown');
 export const mdxFilePaths = fs.readdirSync(MDX_PATH);
 export async function getMdxData(fileName: string) {
     const source = fs.readFileSync(path.join(MDX_PATH, fileName), 'utf-8');
-    const mdxSource = await serialize(source, { parseFrontmatter: true });
+    const mdxSource = await serialize(source, {
+        parseFrontmatter: true,
+        mdxOptions: {
+            rehypePlugins: [[rehypePrettyCode, options]],
+        },
+    });
     return mdxSource;
 }
