f06679759dba4577465da027724abf13b5f86672
rdg1029 <wcle3456@gmail.com>
2024-12-05 22:28:17 +0900

    Update post.tag.tsx

--- src/components/post-tag.tsx
+++ src/components/post-tag.tsx
@@ -11,7 +11,7 @@ export default function PostTag({tag, tagList, setTag, updatePostList}: Props) {
     return (
         <>
             <p className="text-3xl">Tags</p>
-            <div className='flex flex-wrap gap-2 max-w-48 m-3'>
+            <div className='flex flex-wrap gap-2 md:max-w-48 m-3'>
                 <button
                     className={`rounded-xl ${tag === "ALL" ? 'bg-neutral-400' : 'bg-neutral-200 hover:bg-neutral-300'} text-sm font-semibold py-0.5 px-1`}
                     onClick={() => {
